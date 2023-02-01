fd3496645b7d313ae34d4b1b0821e9def9f46bde
frangel13v <f.r.angel13v@gmail.com>
2023-02-01 11:37:16 +0100

    Initial import

---
+++ start-database.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker run \
+--detach \
+--name some-mariadb \
+--env MARIADB_USER=example-user \
+--env MARIADBP_PASSWORD=my_cool_secret \
+--env MARIADB_ROOT_PASSWORD=my-secret-pw \
+-p 3306:3306 \
+-v $PWD/mariadb-data:/var/lib/mysql mariadb:latest
